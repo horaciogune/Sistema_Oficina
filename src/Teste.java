a58089bddcdc2176f8bea0b8fd5ab8026f155d7f
Dev True <165159945+horaciogune@users.noreply.github.com>
2024-08-26 21:18:20 +0100

    comite pelo git

--- src/Teste.java
+++ src/Teste.java
@@ -11,7 +11,7 @@
 public class Teste {
     
     public static void main(String[] args) {
-        System.out.println(" Ola mundo");
+        System.out.println(" Ola mundo" vamos programar);
     }
     
 }
